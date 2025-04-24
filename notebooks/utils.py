3c989602fd7994695e410c49bdfc2341f1bb5102
willsmithDB <willsmithDB>
2025-04-24 18:11:54 +0000

    Updated public version of repo
    Updated 8b and 70b model parallelism with accelerate. Single node with 8 bit quantization as well as normal batch using huggingface

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -24,3 +24,24 @@ def test_load_gpu(dims: tuple = (30000,30000), device: str = "cuda:0"):
     del t 
     torch.cuda.empty_cache()
     print_gpu_utilization()
+
+def batch_inference(encoding, model, tokenizer, terminators, max_tokens: int = 20):
+  import timeit
+
+  start_time = timeit.default_timer()
+
+  with torch.no_grad():
+      generated_ids = model.generate(
+          **encoding,
+          pad_token_id = tokenizer.pad_token_id,
+          max_new_tokens=max_tokens,
+          eos_token_id=terminators,
+          do_sample=True,
+          temperature=0.6,
+          top_p=0.9,
+      )
+
+  batch_generation_elapsed = timeit.default_timer() - start_time
+  print(f"Time taken: {batch_generation_elapsed} seconds")
+
+  return generated_ids
